1d699b10d81a2e2cc332efd58e0957a24b1f5087
Tim Styrlund <tstyrlun@asu.edu>
2019-10-09 22:04:43 -0700

    Initial version of AddingMachine class.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+/**
+ * The AddingMachine class allows AddingMachine objects to add, subtract, 
+ * retrieve, print, and clear the total.
+ * 
+ * @author Tim Styrlund <p>
+ * ASUID: 1210810897 <p>
+ * Assignment: 2
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	 /**
+	  * Initializes a new AddingMachine object which is represented by an
+	  * integer equal to 0
+	  */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the current total of the AddingMachine object
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the parameter to the total of the AddingMachine object
+	 * @param value Value to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the parameter to the total of the AddingMachine object
+	 * @param value Value to be subtracted to the total
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * Returns the history of operations of the AddingMachine object
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Sets the current total to zero 
+	 */
+	public void clear() {
+	
+	}
+}
